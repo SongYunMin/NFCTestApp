86b120ce10007e740c3228e6fc8f90608a18f6e6
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-03-05 02:24:01 +0900

    Read Button, Write Button Listener 생성

--- app/src/main/java/com/example/nfctestapp/MainActivity.java
+++ app/src/main/java/com/example/nfctestapp/MainActivity.java
@@ -4,16 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button readBT = (Button)findViewById(R.id.readbutton);
+    Button writeBT = (Button)findViewById(R.id.writebutton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readBT.setText(R.string.READ);
+        writeBT.setText(R.string.WRITE);
+
+        readBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 구현 중
+                //Intent intent = new Intent(getBaseContext(),)
+            }
+        });
     }
 
     @Override
@@ -28,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 for(int i = 0; i < rawMessages.length;i++){
                     messages[i] = (NdefMessage) rawMessages[i];
                 }
-
+                NdefRecord rtdUriRecord1 =
+                        NdefRecord.createUri("210.124.110.96/Ticket_Value.php");
             }
         }
     }
