63f2a7489020cee5b619c3e3c0d7f93a8654e2dc
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-03-04 19:36:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nfctestapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nfctestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
