5451be84b3a78ab6f86676ad02e5b4160a0c3e53
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-03-04 19:55:36 +0900

    Google Developers NFC Example Code

--- app/src/main/java/com/example/nfctestapp/MainActivity.java
+++ app/src/main/java/com/example/nfctestapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.nfctestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())){
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if(rawMessages != null){
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for(int i = 0; i < rawMessages.length;i++){
+                    messages[i] = (NdefMessage) rawMessages[i];
+                }
+
+            }
+        }
     }
 }
