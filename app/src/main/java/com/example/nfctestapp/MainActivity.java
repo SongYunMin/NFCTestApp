1983a7a305bd2f6b7ba265deda6f402c57cbfa90
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-04-19 01:32:23 +0900

    NFC 카드 에뮬레이터 테스트

--- app/src/main/java/com/example/nfctestapp/MainActivity.java
+++ app/src/main/java/com/example/nfctestapp/MainActivity.java
@@ -12,41 +12,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button readBT = (Button)findViewById(R.id.readbutton);
-    Button writeBT = (Button)findViewById(R.id.writebutton);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        readBT.setText(R.string.READ);
-        writeBT.setText(R.string.WRITE);
 
-        readBT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 구현 중
-                //Intent intent = new Intent(getBaseContext(),)
-            }
-        });
     }
 
-    @Override
-    protected void onNewIntent(Intent intent){
-        super.onNewIntent(intent);
-
-        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())){
-            Parcelable[] rawMessages =
-                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if(rawMessages != null){
-                NdefMessage[] messages = new NdefMessage[rawMessages.length];
-                for(int i = 0; i < rawMessages.length;i++){
-                    messages[i] = (NdefMessage) rawMessages[i];
-                }
-                NdefRecord rtdUriRecord1 =
-                        NdefRecord.createUri("210.124.110.96/Ticket_Value.php");
-            }
-        }
-    }
 }
